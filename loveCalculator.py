edd1c45bb9f9950add20e84c916f7a80e1160752
Galardo Sugito <galardosugito94@gmail.com>
2022-02-10 21:20:00 +0700

    Penambahan komen
    Line 13

--- loveCalculator.py
+++ loveCalculator.py
@@ -10,7 +10,7 @@
 
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
-name2 = input("What is your partner's name? \n")
+name2 = input("What is your partner's name? \n") # Tambahan komen disini
 
 
 name1_lower = name1.lower()
